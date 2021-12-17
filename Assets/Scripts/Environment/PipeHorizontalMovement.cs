5a09130b374cf310a1571e7e266b04d99d9e1fc3
kadok <caengort@protonmail.com>
2021-12-17 12:11:41 +0300

    Added pipe movement.

--- Assets/Scripts/Environment/PipeHorizontalMovement.cs
+++ Assets/Scripts/Environment/PipeHorizontalMovement.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 namespace FlappyClone.Environment
 {
+    // I never know how to name those scripts.
     public class PipeHorizontalMovement : MonoBehaviour
     {
-        
+
+        [SerializeField] private float speed = 5f;
+        private Transform _tf;
+
+        private void Start()
+        {
+            TryGetComponent(out _tf);
+        }
+
+        private void Update()
+        {
+            _tf.Translate(Vector3.left * speed * Time.deltaTime); // It just works.
+        }
     }
 }

e6fbb6107c9a8bdea5839d7088d681986c45f9eb
kadok <caengort@protonmail.com>
2021-12-18 13:48:22 +0300

    Death script rewrite.

---
+++ Assets/Scripts/Core/DeathToggle.cs
@@ -0,0 +1,46 @@
+using System;
+using FlappyClone.Environment;
+using FlappyClone.Player;
+using UnityEngine;
+
+namespace FlappyClone.Core
+{
+    public class DeathToggle : MonoBehaviour
+    {
+        public event Action OnDeath;
+
+        [SerializeField] private ObstacleDetector detector;
+        [SerializeField] private Flight flight;
+
+        private bool _isDead;
+
+        private void OnEnable()
+        {
+            detector.OnObstacleCollision += Die;
+        }
+
+        private void OnDisable()
+        {
+            detector.OnObstacleCollision -= Die;
+        }
+
+        // TODO: this is trash.
+        private void Die()
+        {
+            if(_isDead) return;
+            _isDead = true;
+            flight.enabled = false;
+            foreach (var o in FindObjectsOfType(typeof(PipeHorizontalMovement), true))
+            {
+                var pipe = (PipeHorizontalMovement) o;
+                pipe.enabled = false;
+            }
+            foreach (var o in FindObjectsOfType(typeof(BackgroundScroll), true))
+            {
+                var scroll = (BackgroundScroll) o;
+                scroll.enabled = false;
+            }
+            OnDeath?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Environment/PipeDespawner.cs
+++ Assets/Scripts/Environment/PipeDespawner.cs
@@ -1,4 +1,3 @@
-using System;
 using FlappyClone.Core;
 using UnityEngine;
 
